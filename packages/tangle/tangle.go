4f4999e69e45c618d315c5e0a30a19a8a4efc928
capossele <angelocapossele@gmail.com>
2021-07-01 10:09:55 +0100

    disable scheduler

--- packages/tangle/tangle.go
+++ packages/tangle/tangle.go
@@ -14,7 +14,6 @@ import (
 	"github.com/iotaledger/hive.go/identity"
 	"github.com/iotaledger/hive.go/kvstore"
 	"github.com/iotaledger/hive.go/kvstore/mapdb"
-	"github.com/iotaledger/hive.go/timeutil"
 	"github.com/iotaledger/hive.go/typeutils"
 	"github.com/mr-tron/base58"
 
@@ -132,38 +131,38 @@ func (t *Tangle) Setup() {
 	}))
 
 	// we are never using the FIFOScheduler when the node is started in a synced state
-	t.fifoScheduling.SetTo(!t.Synced())
+	// t.fifoScheduling.SetTo(!t.Synced())
 
 	// start the corresponding scheduler
-	if t.fifoScheduling.IsSet() {
-		t.FIFOScheduler.Start()
-	} else {
-		t.Scheduler.Start()
-	}
+	// if t.fifoScheduling.IsSet() {
+	t.FIFOScheduler.Start()
+	// } else {
+	// 	t.Scheduler.Start()
+	// }
 
 	// pass solid messages to the scheduler
 	t.Solidifier.Events.MessageSolid.Attach(events.NewClosure(t.schedule))
 
-	var switchSchedulerOnce sync.Once
-	// switch the scheduler, the first time we get synced
-	t.TimeManager.Events.SyncChanged.Attach(events.NewClosure(func(e *SyncChangedEvent) {
-		// only switch, if we became synced and the FIFO scheduler is currently running
-		if !e.Synced || !t.fifoScheduling.IsSet() {
-			return
-		}
-		switchSchedulerOnce.Do(func() {
-			// switching the scheduler takes some time, so we must not do it directly in the event func
-			go func() {
-				// delay the actual switch by the sync time windows, the SyncChange event will be triggered
-				// at least this duration before the most recent messages have been solidified
-				if timeutil.Sleep(t.Options.SyncTimeWindow, t.shutdownSignal) {
-					t.fifoScheduling.SetTo(false) // stop adding new messages to the FIFO scheduler
-					t.FIFOScheduler.Shutdown()    // schedule remaining messages
-					t.Scheduler.Start()           // start the actual scheduler
-				}
-			}()
-		})
-	}))
+	// var switchSchedulerOnce sync.Once
+	// // switch the scheduler, the first time we get synced
+	// t.TimeManager.Events.SyncChanged.Attach(events.NewClosure(func(e *SyncChangedEvent) {
+	// 	// only switch, if we became synced and the FIFO scheduler is currently running
+	// 	if !e.Synced || !t.fifoScheduling.IsSet() {
+	// 		return
+	// 	}
+	// 	switchSchedulerOnce.Do(func() {
+	// 		// switching the scheduler takes some time, so we must not do it directly in the event func
+	// 		go func() {
+	// 			// delay the actual switch by the sync time windows, the SyncChange event will be triggered
+	// 			// at least this duration before the most recent messages have been solidified
+	// 			if timeutil.Sleep(t.Options.SyncTimeWindow, t.shutdownSignal) {
+	// 				t.fifoScheduling.SetTo(false) // stop adding new messages to the FIFO scheduler
+	// 				t.FIFOScheduler.Shutdown()    // schedule remaining messages
+	// 				t.Scheduler.Start()           // start the actual scheduler
+	// 			}
+	// 		}()
+	// 	})
+	// }))
 }
 
 // ProcessGossipMessage is used to feed new Messages from the gossip layer into the Tangle.
@@ -237,14 +236,14 @@ func (t *Tangle) Shutdown() {
 // schedule schedules the message with the given id.
 func (t *Tangle) schedule(id MessageID) {
 	// during bootstrapping use the FIFOScheduler for everything
-	if t.fifoScheduling.IsSet() {
-		t.FIFOScheduler.Schedule(id)
-		return
-	}
+	// if t.fifoScheduling.IsSet() {
+	t.FIFOScheduler.Schedule(id)
+	// return
+	// }
 
-	if err := t.Scheduler.SubmitAndReady(id); err != nil {
-		t.Events.Error.Trigger(errors.Errorf("failed to submit to scheduler: %w", err))
-	}
+	// if err := t.Scheduler.SubmitAndReady(id); err != nil {
+	// 	t.Events.Error.Trigger(errors.Errorf("failed to submit to scheduler: %w", err))
+	// }
 }
 
 // endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
